5ab55717e1b9aa905f3c73dd68960802d946fe3f
tiago1820 <tiago.zdo@gmail.com>
2023-10-07 15:02:31 -0300

    Refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import Quote from './components/Quote/Quote';
 import quotes from './data';
 import './App.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
+
+    const [num, setNum] = useState(0);
 
     const randomNumber = (quotes) => {
         return Math.floor(Math.random() * quotes.length);
     }
 
-    const num = randomNumber(quotes);
+    useEffect(() => {
+        setNum(randomNumber(quotes))
+    }, [num])
 
     return (
-        <Quote 
+        <Quote
             quote={quotes[num].quote}
             author={quotes[num].author}
+            setNum={setNum}
+            randomNumber={randomNumber}
+            quotes={quotes}
         />
     )
 }
