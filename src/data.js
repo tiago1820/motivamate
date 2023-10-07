e1f15642d9fc3f3c42ecc02642a8e0dc6b48f919
tiago1820 <tiago.zdo@gmail.com>
2023-10-07 18:23:38 -0300

    Nueva frase agregada

--- src/data.js
+++ src/data.js
@@ -3,5 +3,6 @@ export default [
     { quote: 'Si somos libres, todo nos sobra.', author: 'José de San Martín' },
     { quote: 'El único modo de hacer un gran trabajo es amar lo que haces.', author: 'Steve Jobs' },
     { quote: 'No dejes que la opinión de otras personas ahogue tu propia voz', author: 'Steve Jobs' },
+    { quote: 'Deja de decir "más tarde". NO lo harás más tarde. Hazlo o decide no hacerlo.', author: 'Chris Brogan' },
 
 ]
